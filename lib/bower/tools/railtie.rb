647124d4bff5dd0a37d940188ac351fd1213d922
Will Fisher <will@teknofire.net>
2013-12-31 14:55:41 -0900

    fixing but with adding asset configuration to rails 3.2

--- lib/bower/tools/railtie.rb
+++ lib/bower/tools/railtie.rb
@@ -9,11 +9,13 @@ module Bower
         require "bower/tools/install_generator"
       end
       
-      config.assets.precompile << lambda do |path, filename|
-        exts = %w(.sh .html .json .txt .md .js .css .license .lock)
-        exclude_paths = /(tests|test|doc|example|examples|tasks)/
-        bower_components_path = File.dirname(path).gsub(Rails.root.join('vendor/assets/bower_components').to_s, '')
-        filename =~ /vendor\/assets\/bower_components/ && !File.extname(path).blank? && !(bower_components_path =~ exclude_paths) && !exts.include?(File.extname(path).downcase)
+      config.to_prepare do
+        Rails.configuration.assets.precompile << lambda do |path, filename|
+          exts = %w(.sh .html .json .txt .md .js .css .license .lock)
+          exclude_paths = /(tests|test|doc|example|examples|tasks)/
+          bower_components_path = File.dirname(path).gsub(Rails.root.join('vendor/assets/bower_components').to_s, '')
+          filename =~ /vendor\/assets\/bower_components/ && !File.extname(path).blank? && !(bower_components_path =~ exclude_paths) && !exts.include?(File.extname(path).downcase)
+        end
       end
     end
   end
--- lib/bower/tools/version.rb
+++ lib/bower/tools/version.rb
@@ -1,5 +1,5 @@
 module Bower
   module Tools
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
