490896e44a072df40e5fe383b5f4b8cec8ae20d6
Will Fisher <will@teknofire.net>
2014-01-09 16:14:45 -0900

    only need to modify precompile in rails 4+

--- lib/bower/tools/railtie.rb
+++ lib/bower/tools/railtie.rb
@@ -9,12 +9,15 @@ module Bower
         require "bower/tools/install_generator"
       end
       
-      config.to_prepare do
-        Rails.configuration.assets.precompile << lambda do |path, filename|
-          exts = %w(.sh .html .json .txt .md .js .css .license .lock)
-          exclude_paths = /(tests|test|doc|example|examples|tasks)/
-          bower_components_path = File.dirname(path).gsub(Rails.root.join('vendor/assets/bower_components').to_s, '')
-          filename =~ /vendor\/assets\/bower_components/ && !File.extname(path).blank? && !(bower_components_path =~ exclude_paths) && !exts.include?(File.extname(path).downcase)
+      # Only needed in Rails 4+, it no longer pulls things from the vendor/assets directory automatically
+      if Rails::VERSION::MAJOR >= 4
+        config.to_prepare do
+          Rails.configuration.assets.precompile << lambda do |path, filename|
+            exts = %w(.sh .html .json .txt .md .js .css .license .lock)
+            exclude_paths = /(tests|test|doc|example|examples|tasks)/
+            bower_components_path = File.dirname(path).gsub(Rails.root.join('vendor/assets/bower_components').to_s, '')
+            filename =~ /vendor\/assets\/bower_components/ && !File.extname(path).blank? && !(bower_components_path =~ exclude_paths) && !exts.include?(File.extname(path).downcase)
+          end
         end
       end
     end
--- lib/bower/tools/version.rb
+++ lib/bower/tools/version.rb
@@ -1,5 +1,5 @@
 module Bower
   module Tools
-    VERSION = "0.0.4"
+    VERSION = "0.0.5"
   end
 end
