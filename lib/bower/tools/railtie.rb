072b0360ecc398a90c4fbc8207433a7e3818f5cf
Will Fisher <will@teknofire.net>
2014-03-10 19:32:51 -0800

    fixing some issues with other libs that require compiling

--- lib/bower/tools/railtie.rb
+++ lib/bower/tools/railtie.rb
@@ -13,8 +13,8 @@ module Bower
       if Rails::VERSION::MAJOR >= 4
         config.to_prepare do
           Rails.configuration.assets.precompile << lambda do |path, filename|
-            exts = %w(.sh .html .json .txt .md .js .css .license .lock)
-            exclude_paths = /(tests|test|doc|example|examples|tasks)/
+            exts = %w(.sh .html .json .txt .md .js .css .license .lock .markdown .jade .map)
+            exclude_paths = /(tests|test|doc|example|examples|tasks|node_modules)/
             bower_components_path = File.dirname(path).gsub(Rails.root.join('vendor/assets/bower_components').to_s, '')
             filename =~ /vendor\/assets\/bower_components/ && !File.extname(path).blank? && !(bower_components_path =~ exclude_paths) && !exts.include?(File.extname(path).downcase)
           end
