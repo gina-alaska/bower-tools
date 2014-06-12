806284a76b7cff536bd3cf0c1d6d2c5e12ff625d
Will Fisher <will@teknofire.net>
2014-06-12 09:48:45 -0800

    removing bad exclusions

--- lib/bower/tools/railtie.rb
+++ lib/bower/tools/railtie.rb
@@ -14,7 +14,7 @@ module Bower
         config.to_prepare do
           Rails.configuration.assets.precompile << lambda do |path, filename|
             exts = %w(.sh .html .json .txt .md .js .css .license .lock .markdown .jade .map .py .yml .draw)
-            exclude_paths = /(src|tests|test|doc|docs|example|examples|tasks|ace\-builds|development\-bundle|node_modules)\//
+            exclude_paths = /\/(tests|test|doc|docs|example|examples|kitchen-sink|demo|tasks|development\-bundle|node_modules)\//
             bower_components_path = File.dirname(path).gsub(Rails.root.join('vendor/assets/bower_components').to_s, '')
             filename =~ /vendor\/assets\/bower_components/ && !File.extname(path).blank? && (bower_components_path =~ exclude_paths).nil? && !exts.include?(File.extname(path).downcase)
           end
